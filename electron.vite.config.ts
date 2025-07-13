a4105dfbf2613e8623a5145cf5c581e2e84a775a
GuanMo <13216698987@163.com>
2025-07-13 15:43:30 +0800

    init: 重新初始化项目

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,55 +1,20 @@
-import { resolve } from 'path';
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
-import react from '@vitejs/plugin-react';
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
-    build: {
-      outDir: 'dist/main',
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'apps/electron/src/main.ts'),
-        },
-      },
-    },
-    resolve: {
-      alias: {
-        '@echosoul/common': resolve(__dirname, 'packages/common/src'),
-      },
-    },
+    plugins: [externalizeDepsPlugin()]
   },
   preload: {
-    plugins: [externalizeDepsPlugin()],
-    build: {
-      outDir: 'dist/preload',
-      rollupOptions: {
-        input: {
-          index: resolve(__dirname, 'apps/electron/src/preload.ts'),
-        },
-      },
-    },
-    resolve: {
-      alias: {
-        '@echosoul/common': resolve(__dirname, 'packages/common/src'),
-      },
-    },
+    plugins: [externalizeDepsPlugin()]
   },
   renderer: {
-    root: 'apps/renderer',
-    plugins: [react()],
     resolve: {
       alias: {
-        '@': resolve(__dirname, 'apps/renderer/src'),
-        '@echosoul/common': resolve(__dirname, 'packages/common/src'),
-      },
+        '@renderer': resolve('src/renderer/src')
+      }
     },
-    build: {
-      rollupOptions: {
-        input: {
-          index: resolve(__dirname, 'apps/renderer/index.html'),
-        },
-      },
-    },
-  },
-});
+    plugins: [vue()]
+  }
+})
